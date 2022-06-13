07720fc89d0fe35b3a09f8f0c8f5189cd82c9c3b
edwilson543 <edward.e.j.wilson@gmail.com>
2022-06-13 08:58:43 +0100

    Fixed a bug - it's not valid to use symmetry when we want the win location, so made update not to cache symmetry class automatically when this is the case

--- game/app/win_check_cache_decorators.py
+++ game/app/win_check_cache_decorators.py
@@ -6,7 +6,7 @@ best one that speeds it up.
 # Standard library imports
 from collections import OrderedDict
 from functools import lru_cache, update_wrapper, wraps
-from typing import Tuple, List, Callable, Set
+from typing import Tuple, List, Callable, Set, Union
 
 # Third party imports
 import numpy as np
@@ -43,8 +43,8 @@ class LRUCacheWinSearch:
         self.hash_key_kwargs = hash_key_kwargs
         self.cache_maxsize = maxsize
         self.use_symmetry = use_symmetry
-        self.win_search_func = None
-        self.cache = OrderedDict({})
+        self.win_search_func: Union[None, Callable] = None  # PyCharm linter doesn't like None | Callable
+        self.cache: OrderedDict = OrderedDict({})
 
     def __call__(self, win_search_func: Callable = None, *args, **kwargs):
         """
@@ -68,19 +68,24 @@ class LRUCacheWinSearch:
         elif win_search_func is None and self.win_search_func is not None:
             return self._get_search_return_value(*args, **kwargs)
 
-    def _get_search_return_value(self, *args, **kwargs) -> (bool, List[Tuple[int]]):
+    def _get_search_return_value(self, *args, **kwargs) -> Tuple[bool, List[Tuple[int]] | None]:
         """
         Method to retrieve a value from the cache if available, or call the search function and then cache the
         return if it is not available.
-        Returns: Equivalent to the win_check_and_location_search method
+        Note that if self.use_symmetry is set to True, then all arrays symmetrically equivalent to the
+        passed playing grid are automatically cached against the same return value, when "get_win_location" is not True,
+        otherwise using symmetry is invalid.
+
+        Parameters/Returns: As for the win_check_and_location_search method
         """
         hash_key = self._create_hash_key_from_kwargs(*args, **kwargs)
         if hash_key in self.cache:
             self.cache.move_to_end(hash_key)  # Now the most recently used
             return self.cache[hash_key]
-        else:
-            search_return_value = self.win_search_func(*args, **kwargs)  # Must directly call function and cache
-            if self.use_symmetry:
+        else:  # Must directly call function and cache
+            search_return_value = self.win_search_func(*args, **kwargs)
+            if self.use_symmetry and not kwargs["get_win_location"]:
+                # note the not kwargs["get_win_location"] is to avoid symmetry returning the wrong win location
                 hash_key_list = self._create_hash_key_list_for_symmetry_set_from_kwargs(*args, **kwargs)
                 for symm_hash_key in hash_key_list:
                     self._cache_return_value(hash_key=symm_hash_key, return_value=search_return_value)
@@ -93,8 +98,6 @@ class LRUCacheWinSearch:
         Method to cache the passed return_value with the passed hash_key, and if the maximum size of the cache
         is exceeded, remove the least recently used item. Note that return_value becomes the most recently used item.
         """
-        #  TODO a clever thing here would be to add the symmetric equivalence class of the board - may or may not
-        # speed it up though
         self.cache[hash_key] = return_value
         self.cache.move_to_end(key=hash_key)  # Now the most recently used
         if self.cache_maxsize is not None and len(self.cache) > self.cache_maxsize:
@@ -122,17 +125,23 @@ class LRUCacheWinSearch:
         """
         Method to create a list of hash keys from kwargs for each tuple in the symmetry set of a given playing grid.
         These can then be used to cache all of these different tuples against the same return value.
+
+        Returns: A list of hash_keys based on the hash_key_kwargs instance attribute.
         """
         if "playing_grid" not in kwargs:
             raise KeyError("Attempted to call _create_list_of_hash_keys_from_kwargs with kwargs that do not"
-                           f"include 'playing_grid. kwargs: {kwargs}")
+                           f"include 'playing_grid'. kwargs: {kwargs}")
+        if "get_win_location" not in kwargs:
+            raise KeyError("Attempted to call _create_list_of_hash_keys_from_kwargs with kwargs that do not"
+                           f"include 'get_win_location'. kwargs: {kwargs}")
         else:
             playing_grid: np.ndarray = kwargs["playing_grid"]
             symmetry_set: Set[Tuple] = get_symmetry_set_of_tuples_from_array(array=playing_grid)
             hash_key_list = []
             for symmetric_tup in symmetry_set:
                 kwargs["playing_grid"] = symmetric_tup
-                hash_key_list.append(self._create_hash_key_from_kwargs(*args, **kwargs))
+                hash_key = self._create_hash_key_from_kwargs(*args, **kwargs)
+                hash_key_list.append(hash_key)
             return hash_key_list
 
 
